e84422728ad3502b18d82000b7fe896f31fe12a8
STEPIC <stepicproduction@gmail.com>
2025-12-26 15:28:44 +0100

    juste petit changement

--- actualites/serializers.py
+++ actualites/serializers.py
@@ -4,7 +4,7 @@ from .models import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
     user = UtilisateurSerializer(read_only=True)
-    datePub = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    datePub = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%s", read_only=True)
 
     class Meta:
         model = Blog
