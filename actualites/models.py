3f148117e055ef50ffc68ddf9a02d55075f52adf
STEPIC <stepicproduction@gmail.com>
2025-12-30 19:04:53 +0100

    changement en timezone

--- actualites/models.py
+++ actualites/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 class Blog(models.Model):
     titreActu = models.CharField(max_length=50)
     contenuActu = models.TextField()
     imageActu = models.ImageField(upload_to="actualites/", null=True, blank=True)
-    datePub = models.DateTimeField(auto_now_add=True)
+    datePub = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="Actualite_crees")
 
--- presse/models.py
+++ presse/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 class Presse(models.Model):
     titre = models.CharField(max_length=200)
     source = models.CharField(max_length=200, null=True, blank=True)
     contenu = models.TextField()
     image = models.ImageField(upload_to="presse/", blank=True, null=True)
-    date_pub = models.DateTimeField(auto_now_add=True)
+    date_pub = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="Presse_crees")
 
     def __str__(self):
