4e70268bd0eced244dd695c7e2a6c525e7fb6b1f
STEPIC <stepicproduction@gmail.com>
2026-01-09 18:35:43 +0100

    un petit changement

--- django_project/urls.py
+++ django_project/urls.py
@@ -63,7 +63,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('chat-assistant/', chat_assistant, name='chat-assistant'), # Route pour l'IA
+    path('api/chat-assistant/', chat_assistant, name='chat-assistant'), # Route pour l'IA
 ]
 
 if settings.DEBUG:
